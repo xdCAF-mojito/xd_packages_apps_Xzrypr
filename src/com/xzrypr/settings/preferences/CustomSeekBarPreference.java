92d32d0283392e5e7fbb231db4f0370781e65704
Pranav Vashi <neobuddy89@gmail.com>
2019-02-11 02:03:16 +0530

    xzrypr: Add Custom Seekbar preferences
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- src/com/xzrypr/settings/preferences/CustomSeekBarPreference.java
+++ src/com/xzrypr/settings/preferences/CustomSeekBarPreference.java
@@ -1,17 +1,16 @@
-/*
- * Copyright (C) 2016 The Dirty Unicorns Project
+/* * Copyright (C) 2017 ColtOS Android
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License
+ * limitations under the License.
  */
 
 package com.xzrypr.settings.preferences;
@@ -38,7 +37,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
     private int mInterval = 1;
     private int mCurrentValue;
     private int mDefaultValue = -1;
-    private int mMax = 100;
+    private int mMax = 255;
     private String mUnits = "";
     private String mDefaultText = "";
     private SeekBar mSeekBar;
@@ -51,12 +50,9 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         final TypedArray a = context.obtainStyledAttributes(
                 attrs, R.styleable.CustomSeekBarPreference);
 
-        mMax = attrs.getAttributeIntValue(ANDROIDNS, "max", 100);
+        mMax = attrs.getAttributeIntValue(ANDROIDNS, "max", 255);
         mMin = attrs.getAttributeIntValue(SETTINGS_NS, "min", 0);
         mDefaultValue = attrs.getAttributeIntValue(ANDROIDNS, "defaultValue", -1);
-        if (mDefaultValue > mMax) {
-            mDefaultValue = mMax;
-        }
         mUnits = getAttributeStringValue(attrs, SETTINGS_NS, "units", "");
         mDefaultText = getAttributeStringValue(attrs, SETTINGS_NS, "defaultText", "Def");
 
@@ -148,10 +144,9 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         mSeekBar.setProgress(mCurrentValue - mMin);
         mTitle = (TextView) view.findViewById(android.R.id.title);
 
+        mSeekBar.setEnabled(isEnabled());
         view.setDividerAllowedAbove(false);
         //view.setDividerAllowedBelow(false);
-
-        mSeekBar.setEnabled(isEnabled());
     }
 
     public void setMax(int max) {
@@ -236,7 +231,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         if (mDefaultValue > mMax) {
             mDefaultValue = mMax;
         }
-        if (mCurrentValue == mDefaultValue) {
+        if (mCurrentValue == mDefaultValue && mStatusText != null) {
             mStatusText.setText(mDefaultText);
         }
     }
