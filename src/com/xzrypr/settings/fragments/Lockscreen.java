8fc565f672dfcb36fc847f89bbcf97b37477c01e
xyzuan <xyzuannihboss@gmail.com>
2021-08-16 15:57:32 +0700

    xzrypr: Lockscreen: Change Clock Face preferences implementation

--- src/com/xzrypr/settings/fragments/Lockscreen.java
+++ src/com/xzrypr/settings/fragments/Lockscreen.java
@@ -15,16 +15,10 @@
  */
 package com.xzrypr.settings.fragments;
 
-import static android.os.UserHandle.USER_CURRENT;
-
 import android.os.Bundle;
 import android.content.Context;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import android.provider.Settings;
-
-import androidx.preference.*;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto;
 
@@ -32,64 +26,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class Lockscreen extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
-
-    private static final String CUSTOM_CLOCK_FACE = Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE;
-    private static final String DEFAULT_CLOCK = "com.android.keyguard.clock.DefaultClockController";
-
-    private ListPreference mLockClockStyles;
-    private Context mContext;
+public class Lockscreen extends SettingsPreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mContext = getActivity();
-
-        mLockClockStyles = (ListPreference) findPreference(CUSTOM_CLOCK_FACE);
-        String mLockClockStylesValue = getLockScreenCustomClockFace();
-        mLockClockStyles.setValue(mLockClockStylesValue);
-        mLockClockStyles.setSummary(mLockClockStyles.getEntry());
-        mLockClockStyles.setOnPreferenceChangeListener(this);
-
         addPreferencesFromResource(R.xml.xzrypr_settings_lockscreen);
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mLockClockStyles) {
-            setLockScreenCustomClockFace((String) newValue);
-            int index = mLockClockStyles.findIndexOfValue((String) newValue);
-            mLockClockStyles.setSummary(mLockClockStyles.getEntries()[index]);
-            return true;
-        }
-        return false;
-    }
-
-    private String getLockScreenCustomClockFace() {
-        String value = Settings.Secure.getStringForUser(mContext.getContentResolver(),
-                CUSTOM_CLOCK_FACE, USER_CURRENT);
-
-        if (value == null || value.isEmpty()) value = DEFAULT_CLOCK;
-
-        try {
-            JSONObject json = new JSONObject(value);
-            return json.getString("clock");
-        } catch (JSONException ex) {
-        }
-        return value;
-    }
-
-    private void setLockScreenCustomClockFace(String value) {
-        try {
-            JSONObject json = new JSONObject();
-            json.put("clock", value);
-            Settings.Secure.putStringForUser(mContext.getContentResolver(), CUSTOM_CLOCK_FACE,
-                    json.toString(), USER_CURRENT);
-        } catch (JSONException ex) {
-        }
     }
 
     @Override
